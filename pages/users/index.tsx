bfe564e50d2f66e461c92efeac0ca5543c45ffa8
Deni Sugiarto <sugiarto.deni@gmail.com>
2021-10-11 07:32:21 +0700

    final project

---
+++ pages/users/index.tsx
@@ -0,0 +1,46 @@
+import { useRouter } from "next/router";
+import Layout from "../../components/Layout";
+import styles from "../../styles/Users.module.css";
+
+interface UsersProps {
+  dataUsers: Array<any>;
+}
+
+export default function Users(props: UsersProps) {
+  const { dataUsers } = props;
+  const router = useRouter();
+  return (
+    <Layout pageTitle="Users Page">
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          flexWrap: "wrap",
+        }}
+      >
+        {dataUsers.map((user) => {
+          return (
+            <div
+              key={user.id}
+              onClick={() => router.push(`/users/${user.id}`)}
+              className={styles.card}
+            >
+              <p>{user.name}</p>
+              <p>{user.email}</p>
+            </div>
+          );
+        })}
+      </div>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const dataUsers = await res.json();
+  return {
+    props: {
+      dataUsers,
+    },
+  };
+}
